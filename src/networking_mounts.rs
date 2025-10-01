c3f8e0522bf4c77402a374026312a51b4a776fc5
Ivan Liang <r33t@cmu.edu>
2025-10-01 12:49:32 -0400

    fix: whitespacing

--- src/networking_mounts.rs
+++ src/networking_mounts.rs
@@ -14,10 +14,11 @@ pub(crate) fn modify_networking_mounts(spec: &mut Spec) {
             match mount.destination().to_str() {
                 Some("/etc/hosts") | Some("/etc/hostname") | Some("/etc/resolv.conf") => {
                     if let Some(options) = mount.options()
-                        && !options.contains(&"ro".into()) {
-                            let mut options = options.clone();
-                            options.push("ro".into());
-                            mount.set_options(Some(options));
+                        && !options.contains(&"ro".into())
+                    {
+                        let mut options = options.clone();
+                        options.push("ro".into());
+                        mount.set_options(Some(options));
                     }
                 }
                 Some(_) => {}
